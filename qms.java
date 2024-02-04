1feab1f90bdb7cc3e2b81b4e383d07c42caf8e0b
Cloud <34744401+ahexki@users.noreply.github.com>
2024-02-04 22:18:23 +0630

    second

--- qms.java
+++ qms.java
@@ -1,7 +1,8 @@
 import java.util.*;
 
-public class Main {
+public class qms {
     public static void main(String[] args) {
       System.out.println("Quiz Management System");
+      System.out.println("Testing 1 and 2");
   }
 }
