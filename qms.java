af136e8a045fd36de0a1f60611a74f1f1dddb778
Cloud <34744401+ahexki@users.noreply.github.com>
2024-02-04 22:19:47 +0630

    second to first

--- qms.java
+++ qms.java
@@ -3,6 +3,6 @@ import java.util.*;
 public class qms {
     public static void main(String[] args) {
       System.out.println("Quiz Management System");
-      System.out.println("Testing 1 and 2");
+
   }
 }
